ec45a987f71bbaf6ebf1db9bb00289a3e44c1981
sharmapalani <sharmapalani95@gmail.com>
2025-03-20 02:34:33 +0000

    Powershell script

--- azure-automation.ps1
+++ azure-automation.ps1
@@ -1,9 +1,38 @@
-# Connect to Azure Account
+# Connect to Azure
 Connect-AzAccount
 
-# Get all Azure resource groups
-$resourceGroups = Get-AzResourceGroup
+# Set up variables for VM creation
+$resourceGroupName = "MyResourceGroup"
+$location = "East US"
+$vmName = "MyVM"
+$vmSize = "Standard_DS1_v2"
+$adminUsername = "azureuser"
+$adminPassword = ConvertTo-SecureString "YourPassword123!" -AsPlainText -Force
+$subnetName = "default"
+$vnetName = "MyVNet"
+$storageAccountName = "mystorageacct12345"
 
-# Display the resource groups
-$resourceGroups | Format-Table -AutoSize
+# Create Resource Group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create Virtual Network and Subnet
+$subnetConfig = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix "10.0.0.0/24"
+$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Location $location -Name $vnetName -AddressPrefix "10.0.0.0/16" -Subnet $subnetConfig
+
+# Create Public IP Address
+$publicIp = New-AzPublicIpAddress -ResourceGroupName $resourceGroupName -Location $location -Name "$vmName-PublicIP" -AllocationMethod Dynamic
+
+# Create Network Interface Card (NIC)
+$nic = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name "$vmName-NIC" -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $publicIp.Id
+
+# Create Virtual Machine Configuration
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize $vmSize
+$vmConfig = Set-AzVMOperatingSystem -VM $vmConfig -Windows -ComputerName $vmName -Credential (New-Object System.Management.Automation.PSCredential($adminUsername, $adminPassword))
+$vmConfig = Set-AzVMSourceImage -VM $vmConfig -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Sku "2019-Datacenter" -Version "latest"
+$vmConfig = Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
+
+# Create the Virtual Machine
+New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
+
+Write-Output "Virtual Machine '$vmName' created successfully!"
 
